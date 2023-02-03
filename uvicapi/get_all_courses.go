dbef603f3c367038288ae664999ac3ee0ed3aa0e
Hal <haln_01@proton.me>
2023-02-02 20:31:02 -0800

    feat: `GetAllCourses`

---
+++ uvicapi/get_all_courses.go
@@ -0,0 +1,56 @@
+package uvicapi
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+
+	"github.com/valyala/fastjson"
+)
+
+type Course struct {
+	ID    string `json:"id"`
+	Title string `json:"title"`
+}
+
+func (c *UVicAPI) GetAllCourses() ([]byte, error) {
+	res, err := http.Get("https://uvic.kuali.co/api/v1/catalog/courses/5d9ccc4eab7506001ae4c225")
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	var buf bytes.Buffer
+	if _, err := buf.ReadFrom(res.Body); err != nil {
+		return nil, err
+	}
+
+	resJson, err := fastjson.ParseBytes(buf.Bytes())
+	if err != nil {
+		return nil, err
+	}
+
+	courses := []Course{}
+
+	v := resJson.GetArray()
+	for _, v := range v {
+		id, err := v.Get("__catalogCourseId").StringBytes()
+		if err != nil {
+			return nil, err
+		}
+
+		title, err := v.Get("title").StringBytes()
+		if err != nil {
+			return nil, err
+		}
+
+		c := Course{
+			ID:    string(id),
+			Title: string(title),
+		}
+
+		courses = append(courses, c)
+	}
+
+	return json.MarshalIndent(courses, "", "  ")
+}
