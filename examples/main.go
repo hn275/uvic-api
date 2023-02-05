3ab82db0dfd3040c183c83f2cbb624485133d280
Hal <haln_01@proton.me>
2023-02-04 23:14:46 -0800

    fix: clean up examples

--- examples/main.go
+++ examples/main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
-	"time"
 
 	"github.com/hn275/uvic-api/uvicapi"
 )
 
 func main() {
-	startTime := time.Now()
 
 	term := "202301"
+
 	log.Printf("fetching uvic data for term %s\n", term)
 	uvicClient, err := uvicapi.NewAPI(term)
 	if err != nil {
@@ -31,11 +31,8 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		if err := os.WriteFile("./data/GetSection.json", res, 0666); err != nil {
-			log.Printf("ERROR [GetSection]:\n%v\n", time.Since(startTime))
-		} else {
-			log.Printf("DONE [GetSection] in %v\n", time.Since(startTime))
-		}
+
+		write("GetSection", res)
 	}
 
 	// GetAllSections
@@ -44,11 +41,8 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		if err := os.WriteFile("./data/GetAllSections.json", res, 0666); err != nil {
-			log.Printf("ERROR [GetAllSections]:\n%v\n", time.Since(startTime))
-		} else {
-			log.Printf("DONE [GetAllSections] in %v\n", time.Since(startTime))
-		}
+
+		write("GetAllSections", res)
 	}
 
 	// GetCourseDesc
@@ -57,7 +51,7 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		log.Printf("DONE [GetCourseDesc] in %v\n", time.Since(startTime))
+		// TODO: what
 	}
 
 	// GetTerms
@@ -67,11 +61,8 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
-		if err := os.WriteFile("./data/GetTerms.json", res, 0666); err != nil {
-			log.Printf("ERROR [GetTerms]:\n%v", err)
-		} else {
-			log.Printf("DONE [GetTerms] in %v\n", time.Since(startTime))
-		}
+
+		write("GetTerms", res)
 	}
 
 	// GetAllCourses
@@ -80,12 +71,15 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		if err := os.WriteFile("./data/GetAllCourses.json", res, 0666); err != nil {
-			log.Printf("ERROR [GetAllCourses]:\n%v", err)
-		} else {
-			log.Printf("DONE [GetAllCourses] in %v\n", time.Since(startTime))
-		}
-	}
 
-	log.Fatalf("DONE in %v\n", time.Since(startTime))
+		write("GetAllSections", res)
+	}
+}
+
+func write(fnName string, data []byte) {
+	if err := os.WriteFile(fmt.Sprintf("./data/%s.json", fnName), data, 0666); err != nil {
+		log.Printf("ERROR [%s]:\n%v\n", fnName, err)
+	} else {
+		log.Printf("DONE [%s]\n", fnName)
+	}
 }
